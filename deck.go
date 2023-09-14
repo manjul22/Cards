b3c21857a72b05d4399bed4542d4933d7098297c
manjul22 <manjuljain93@gmail.com>
2023-09-14 10:11:29 +0530

    multiple range syntax and multiple return values

--- deck.go
+++ deck.go
@@ -9,6 +9,16 @@ type deck []string
 
 func newDeck() deck {
 	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suite)
+		}
+	}
+
 	return cards
 }
 
@@ -16,4 +26,8 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
