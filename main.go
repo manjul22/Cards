0feb9290fc5aaa52f40a63408b8e66b5753f9cf9
manjul22 <manjuljain93@gmail.com>
2023-09-17 17:47:20 +0530

    embedded structs and structs with receiver functions

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
@@ -15,9 +21,28 @@ func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 	// fmt.Println(alex)
 
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v",alex)
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v",alex)
+
+	jim := person {
+		firstName: "Jim",
+		lastName: "Party",
+		contactInfo: contactInfo{
+			email: "jim@email.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v",p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
