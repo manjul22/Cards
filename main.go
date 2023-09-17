7629857d31c15bb6093ca674ec78376d846b005f
manjul22 <manjuljain93@gmail.com>
2023-09-17 14:26:11 +0530

    structs in go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	// cards := newDeck()
+	// cards.shuffle()
+	// cards.print()
+
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	fmt.Println(alex)
+	fmt.Printf("%+v",alex)
 }
