231e1170ae5e292587bee0858b3e7633d30337e2
manjul22 <manjuljain93@gmail.com>
2023-09-19 20:55:20 +0530

    Maps in Go

--- main.go
+++ main.go
@@ -27,17 +27,30 @@ func main() {
 	// fmt.Println(alex)
 	// fmt.Printf("%+v",alex)
 
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Party",
-		contactInfo: contactInfo{
-			email:   "jim@email.com",
-			zipCode: 94000,
-		},
-	}
+	// jim := person{
+	// 	firstName: "Jim",
+	// 	lastName:  "Party",
+	// 	contactInfo: contactInfo{
+	// 		email:   "jim@email.com",
+	// 		zipCode: 94000,
+	// 	},
+	// }
 
-	jim.updateName("jimmy")
-	jim.print()
+	// jim.updateName("jimmy")
+	// jim.print()
+
+	colors := map[string]string {
+		"red":"#ff0000",
+		"green": "#4f745",
+		"white": "#ffffff",
+	}
+	//var colors map[string]string
+
+	//colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+	// 	delete(colors,"white")
+	//fmt.Println(colors)
+	printMap(colors)
 }
 
 func (p person) print() {
@@ -46,4 +59,10 @@ func (p person) print() {
 
 func (pointerToPerson *person) updateName(newFirstName string) {
 	(*pointerToPerson).firstName = newFirstName
+}
+
+func printMap(c map[string]string) {
+	for color,hex := range c {
+		fmt.Println("Hex code for", color, "is",hex)
+	}
 }
